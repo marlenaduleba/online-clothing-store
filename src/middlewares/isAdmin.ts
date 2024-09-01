c0385b8545d10cc8bf760f7cdbf84d71ccd7cd29
Marlena Duleba <marlena_duleba@o2.pl>
2024-09-01 21:15:54 +0200

    Refactor isAdmin middleware to use CustomError

--- src/middlewares/isAdmin.ts
+++ src/middlewares/isAdmin.ts
@@ -1,5 +1,5 @@
 import { Request, Response, NextFunction } from 'express';
-
+import CustomError from '../errors/CustomError.js';
 /**
  * Middleware function to check if the authenticated user is an admin.
  *
@@ -7,11 +7,12 @@ import { Request, Response, NextFunction } from 'express';
  * @param res - The response object to send an error message if the user is not an admin.
  * @param next - The next middleware function in the stack.
  *
- * @returns Proceeds to the next middleware if the user is an admin, otherwise returns a 403 Forbidden error.
+ * @returns Proceeds to the next middleware if the user is an admin, otherwise passes an error to the error handler.
  */
 export const isAdmin = (req: Request, res: Response, next: NextFunction) => {
   if (!req.user || req.user.role !== 'admin') {
-    return res.status(403).json({ message: 'Forbidden - Admins only' });
+    const error = new CustomError('Forbidden - Admins only', 403);
+    return next(error);
   }
   next();
 };
