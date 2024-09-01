7699b732a38a02fc0fc336d3debebc2873217ad4
Marlena Duleba <marlena_duleba@o2.pl>
2024-09-01 12:12:55 +0200

    Revise Jest configuration for TypeScript and ES Modules support

--- jest.config.ts
+++ jest.config.ts
@@ -1,19 +1,22 @@
-import type { JestConfigWithTsJest } from "ts-jest";
+import type { Config } from 'jest';
+import { defaults } from 'jest-config';
 
-const config: JestConfigWithTsJest = {
+const config: Config = {
   verbose: true,
+  preset: 'ts-jest/presets/js-with-ts-esm', // Use ts-jest presets to handle TypeScript and ES Modules
   transform: {
-    "^.+\\.ts?$": [
-      "ts-jest",
-      {
-        useESM: true,
-      },
-    ],
+    '^.+\\.ts?$': 'ts-jest', // Use ts-jest to transform TypeScript files
   },
-  extensionsToTreatAsEsm: [".ts"],
+  extensionsToTreatAsEsm: ['.ts'], // Treat .ts files as ESM
   moduleNameMapper: {
-    "^(\\.{1,2}/.*)\\.js$": "$1",
+    '^(\\.{1,2}/.*)\\.js$': '$1', // Resolve issues with .js extensions
   },
+  moduleFileExtensions: [...defaults.moduleFileExtensions, 'ts', 'tsx'], // Add ts and tsx extensions
+  testEnvironment: 'node', // Use Node.js test environment
+  transformIgnorePatterns: [
+    '/node_modules/', // Ignore transforming files in node_modules
+    '\\.pnp\\.[^\\/]+$' // Ignore Yarn PnP (Plug'n'Play) files
+  ],
 };
 
 export default config;
