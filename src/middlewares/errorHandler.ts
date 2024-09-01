bebb1d32065c67ffc994cbf2765cb213b9e03388
Marlena Duleba <marlena_duleba@o2.pl>
2024-09-02 01:03:36 +0200

    Refactor by integrating CustomError handling

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,6 +1,8 @@
-import { Request, Response, NextFunction } from "express";
-const chalk = require('chalk');
+// src/middlewares/errorHandler.ts
 
+import { Request, Response, NextFunction } from "express";
+import CustomError from "../errors/CustomError.js";
+const chalk = require("chalk");
 
 /**
  * Middleware function to handle errors in the application.
@@ -12,7 +14,18 @@ const chalk = require('chalk');
  *
  * @returns Sends a JSON response with the error message and status code, or a generic message if no specific error details are provided.
  */
-export const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
-  console.error(chalk.red(err.message || 'An unknown error occurred'));
-  res.status(err.status || 500).json({ message: err.message || 'Internal Server Error' });
+export const errorHandler = (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  if (err instanceof CustomError) {
+    res
+      .status(err.status)
+      .json({ message: err.message, errors: err.errors || [] });
+  } else {
+    console.error(chalk.red(err.message || "An unknown error occurred"));
+    res.status(500).json({ message: err.message || "Internal Server Error" });
+  }
 };
