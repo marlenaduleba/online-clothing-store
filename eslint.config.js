2166f0ac418c95855a57d943a1139a3be6c9c8f3
Marlena Duleba <marlena_duleba@o2.pl>
2024-09-03 07:00:38 +0200

    Fix globals import issue

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,11 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from '@typescript-eslint/eslint-plugin';
+
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  js.configs.recommended,
   {
+    files: ['**/*.{js,mjs,cjs,ts}'],
     languageOptions: {
       globals: {
         ...globals.browser,
@@ -9,12 +14,11 @@ export default [
       ecmaVersion: 2021,
     },
   },
-  pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   {
     rules: {
-      "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/no-unused-vars": "off",
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
     },
   },
 ];
