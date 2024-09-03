03d46c4b27902cc5f3243f1428ff5fd263c6eb8a
Marlena Duleba <marlena_duleba@o2.pl>
2024-09-03 07:03:52 +0200

    Fix ESLint configuration to support TypeScript and resolve issues

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,14 @@
 import js from '@eslint/js';
 import globals from 'globals';
-import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import tsPlugin from '@typescript-eslint/eslint-plugin';
 
 export default [
   js.configs.recommended,
   {
     files: ['**/*.{js,mjs,cjs,ts}'],
     languageOptions: {
+      parser: tsParser,
       globals: {
         ...globals.browser,
         ...globals.node,
@@ -14,11 +16,15 @@ export default [
       ecmaVersion: 2021,
     },
   },
-  ...tseslint.configs.recommended,
   {
+    plugins: {
+      '@typescript-eslint': tsPlugin,
+    },
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
       '@typescript-eslint/no-unused-vars': 'off',
+      'no-undef': 'off', // Disable no-undef for using process and other Node.js globals
+      'no-unused-vars': 'off', // To handle linting on unused variables
     },
   },
 ];
